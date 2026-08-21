5f53c0ade99d94a555d13680f052aaccaa13cd23
garvshr <garvsharma40@gmail.com>
2026-08-21 11:03:40 +0530

    [Garv] Add : Completed Guess the number project

--- JavaScript/chai/guess-the-number/script.js
+++ JavaScript/chai/guess-the-number/script.js
@@ -1,4 +1,4 @@
-const randomNumber = parseInt(Math.random() * 100 + 1);
+let randomNumber = parseInt(Math.random() * 100 + 1);
 const submit = document.querySelector('#subt')
 const userInput = document.querySelector('#guessField')
 const guessSlot = document.querySelector('.guesses')
@@ -13,7 +13,7 @@ let numGuess = 1
 let playGame = true
 
 if(playGame) {
-    submit = addEventListener('click', function(e) {
+    submit.addEventListener('click', function(e) {
         e.preventDefault()
         const guess = parseInt(userInput.value)
         validateGuess(guess)
@@ -57,18 +57,37 @@ function checkGuess(guess) {
 
 function displayGuess(guess) {
     userInput.value = ''
-    guessSlot.innerHTML
+    guessSlot.innerHTML += `${guess} `
+    numGuess++;
+    remaining.innerHTML = `${11 - numGuess}`
 
 }
 
 function displayMessage(message) {
-    // 
+    lowOrHigh.innerHTML = `<h2>${message}</h2>`
+
 }
 
 function newGame() {
-    // 
-}
+    const newGameButton = document.querySelector('#newGame')
+    newGameButton.addEventListener('click', function(e) {
+        randomNumber = parseInt(Math.random() * 100 + 1);
+        prevGuess = [];
+        numGuess = 1;
+        guessSlot.innerHTML = '';
+        remaining.innerHTML = `${11 - numGuess}`
+        userInput.removeAttribute('disabled', '');
+        startOver.removeChild(p)
+        playGame = true;
+    });
+};
 
 function endGame() {
-    // 
+    userInput.value = '';
+    userInput.setAttribute('disabled', '');
+    p.classList.add('button')
+    p.innerHTML = `<h2 id="newGame" >Start new Game</h2>`;
+    startOver.appendChild(p)
+    playGame = false
+    newGame()
 }
